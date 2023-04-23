606e1568cd5e3cf70afb30e37744d865302843ed
AmrutaRatnaparkhi <92331506+AmrutaRatnaparkhi@users.noreply.github.com>
2023-04-23 23:33:00 +0530

    test 6270 - Script Updation Done

--- src/test/java/com/Test_1/MyTest_Login.java
+++ src/test/java/com/Test_1/MyTest_Login.java
@@ -1,5 +1,7 @@
 package com.Test_1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
@@ -10,9 +12,36 @@ import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 public class MyTest_Login {
 	
 	
-	@Test
-	public void test_6270() {
-		System.out.println("Test 6270");
+	public static void main(String[] args) {
+
+		  // Browser Open
+				WebDriverManager.chromedriver().setup();
+				ChromeDriver driver=new ChromeDriver();
+				driver.get("https://www.facebook.com/login/");
+				driver.manage().window().maximize();
+				
+		  // WebElement --> identify -- Locators ---> 8 --> ID
+				
+				WebElement txt_emailAddress=driver.findElement(By.id("email"));
+				txt_emailAddress.sendKeys("Test@gmail.com");
+				
+				
+				WebElement txt_password=driver.findElement(By.id("pass"));
+				txt_password.sendKeys("12345678");
+				
+				WebElement btn_login=driver.findElement(By.id("loginbutton"));
+				btn_login.click();
+				
+				// verify
+				
+				String TitleHomepage=driver.getTitle();
+				
+				if(TitleHomepage.equals("Facebook")) {
+					
+					System.out.println("Test Case Pass");
+				}else {
+					System.out.println("Test Case Fail");
+				}
 	}
 }
 	
